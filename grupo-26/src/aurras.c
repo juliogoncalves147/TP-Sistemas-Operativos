ae76b1fb44f33ecb251da1b54383f4f7daa3ec94
juliogoncalves147 <juliobeites@hotmail.com>
2021-06-12 17:29:54 +0100

    SO HARD

--- grupo-26/src/aurras.c
+++ grupo-26/src/aurras.c
@@ -9,87 +9,79 @@
 #include <sys/wait.h>
 #include <errno.h>
 
-char** start_filters(char** filters){
-	int i = 0;
-	while(i < 5){
-		filters[i++] = NULL;
-	}
-	return filters;
+char **inputDivide(char *input)
+{
+    char **stdi = (char **)malloc(10 * sizeof(char *)); // aloca memória para um array de apontadores para Strings.
+    for (int j = 0; j < 10; j++)
+        stdi[j] = NULL; // inicia todos os apontadores a NULL.
+
+    int i = 0;
+    while (input)
+    {
+        char *aux = strsep(&input, "\n \t"); // ;  // divide a String dada segundo os delimitadores dados.
+        if (aux && aux[0])
+            stdi[i++] = strdup(aux); // aloca espaço e copia a String.
+    }
+
+    return stdi; // retorna o Array de apontadores para Strings
 }
-
-
-char** takeFilters(int argc, char** argv){
-	/* Estrutura Filtros
-	   Array com espaço para 5 filtros.
-	   filter[0] -> alto
-	   filter[1] -> baixo
-	   filter[2] -> eco
-	   filter[3] -> rapido
-	   filter[4] -> lento 
-	*/
-	int i = 4;
-	char** filters = malloc(sizeof(char*) * 5);
-
-	filters = start_filters(filters);
-	
-	while(i < argc){
-		if (strcmp(argv[i], "alto") == 0) filters[0] = strdup("bin/aurrasd-filters/aurrasd-gain-double");
-		else if (strcmp(argv[i], "baixo") == 0) filters[1] = strdup("bin/aurrasd-filters/aurrasd-gain-half");
-		else if (strcmp(argv[i], "eco") == 0) filters[2] = strdup("bin/aurrasd-filters/aurrasd-echo");
-		else if (strcmp(argv[i], "rapido") == 0) filters[3] = strdup("bin/aurrasd-filters/aurrasd-tempo-double");
-		else if (strcmp(argv[i], "lento") == 0) filters[4] = strdup("bin/aurrasd-filters/aurrasd-tempo-half");
-		i++;
-	}
-	return filters;
-}
-
 
 
 int main (int argc, char * argv[]){
 
-    if(argc == 1){
+    if(argc == 1){ // comando de exemplificação de exemplos
 	int p;	  
 	    p = write(1, "./aurras status\n", 17);
 	    if (p == -1) return 0;
 	    p = write(1, "./aurras transform input-filename output-filename filter-id-1 filter-id-2 ...\n", 78);
 	    if (p == -1) return 0;
     }
-    else if(strcmp("status", argv[1]) == 0) {
+    else if(strcmp("status", argv[1]) == 0 && argc == 2) { // comando em que pedidmos o status do servidor
 	    // codigo status servidor 
-	 
+	    if (mkfifo("bin/clientetoserver", 0777) == -1) { // criação do fifo de comunição
+		    if( errno != EEXIST){	    
+		 	  write(1, "Could Not Creat The FIFO\n", 26);
+		  	  return 0;
+		    }
+	    } 
 
+	    
+
+	    int fd = open("bin/clientetoserver", O_WRONLY); // escreve para o fifo
+	    if((write(fd, argv[1], strlen(argv[1])+1)) == -1){
+		    write(1, "Could Not Write To The FIFO\n", 26);
+	   	    return 0;
+	    }
+	    close(fd);
     }
-    else if(strcmp("transform", argv[1]) == 0 && argc > 4){
+    else if(strcmp("transform", argv[1]) == 0 && argc > 4){ // quando é pedido ao servidor a aplicação de filtros
 	    // codigo executar
-	    if (mkfifo("bin/clientetoserver", 0777) == -1) {
+	    if (mkfifo("bin/clientetoserver", 0777) == -1) { // criação do fifo de comunição
 		    if( errno != EEXIST){	    
 		 	  write(1, "Could Not Creat The FIFO\n", 26);
 		  	  return 0;
 		    }
 	    }
-	   
-	    char* comand = strdup(argv[1]);
+
+	    char* comand = strdup(argv[1]); 
 	    char* finput = strdup(argv[2]);
-	    char* foutput = strdup(argv[3]);
-	    char ** filtros = takeFilters(argc, argv); 
+	    char* foutput = strdup(argv[3]); 
 
 
 	    char resposta[2000];
-
+					        	// vamos juntar todos os comandos numa string para escrever no fifo
 	    strcat(resposta, comand);
 	    strcat(strcat(resposta, " "), finput);
 	    strcat(strcat(resposta, " "),foutput);
-	    int i = 0;
-	    while(i < 5 ){
-		    if(filtros[i] != NULL){		
-		    strcat(strcat(resposta, " "), filtros[i]);		
+	    int i = 4;
+	    while(i < argc ){
+		    if(argv[i] != NULL){		
+		    strcat(strcat(resposta, " "), argv[i]);		
 		    }
 		    i++;
 	    }
-
 	    
-	    
-	    int fd = open("bin/clientetoserver", O_WRONLY);
+	    int fd = open("bin/clientetoserver", O_WRONLY); // escreve para o fifo
 	    if (write(fd, resposta, strlen(resposta)+1) == -1){
 		    write(1, "Could Not Write To The FIFO\n", 26);
 	   	    return 0;
--- grupo-26/src/aurrasd.c
+++ grupo-26/src/aurrasd.c
@@ -6,8 +6,19 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
-#include<sys/wait.h>
+#include <sys/wait.h>
+#include <string.h>
 
+//Estrutura de dados para gerir os Filtros Existentes e sua utilização
+typedef struct filtros{
+    char* filtro_name; // nome do filtro, ex: alto, eco
+    char* filtro_exec; // nome do executavel do filtro a aplicar
+    int used;          // quantos processos estão a utilizar este filtro
+    int max_used;      // numero maximo de processos que podem utilizar este filtro ao mesmo tempo
+    int numero_filtros; // numero de filtros que existem diferentes
+} *FILTROS;
+
+// Função que divide o comando enviado pelo cliente num array de strings
 char **inputDivide(char *input)
 {
     char **stdi = (char **)malloc(10 * sizeof(char *)); // aloca memória para um array de apontadores para Strings.
@@ -25,35 +36,226 @@ char **inputDivide(char *input)
     return stdi; // retorna o Array de apontadores para Strings
 }
 
-
-
-int main (int argc, char * argv[]){
-
-
-
-    int fd = open("bin/clientetoserver", O_RDONLY);
-
-    char buffer[1024];
-    int bytes_read = 0;
-
-    while((bytes_read = read(fd, buffer, 1024)) > 0){
-       // write(1, buffer, bytes_read);
-    }
-
-    char** comandos = inputDivide(buffer);
-
-    // teste para ver se recebe os comandos certos
+// Função determina se um certo input é um filtro
+int isFilter(char* comando, FILTROS filtros[]){
     int i = 0;
-    
+    int val = 0;
+    while(i < filtros[0]->numero_filtros && val == 0){
+        if(strcmp(comando, filtros[i]->filtro_name)) val = 1;
+        i++;
+    }
+
+    return val;
+}
+
+// Função que determina quantos filtros vão ser aplicados a um determinado input
+int numisFilter(char** comandos, FILTROS filtros[]){
+    int i = 3;
+    int cont = 0;
     while(comandos[i] != NULL){
-        printf("%s\n", comandos[i++]);
+        if(isFilter(comandos[i], filtros)) cont++;
+        i++;
+    }
+    return cont;
+}
+
+// Função que executa a filtragem de uma musica aquando da chamada do comando transform pelo cliente.
+// config_filename - diretoria onde esta o ficheiro de configuração de filtros
+// filters_folder - diretoria onde se encontram os executaveis dos filtos
+// comandos - array de strings que contem os comandos mandados pelo cliente
+/*
+   Normalmente para esta funcao:
+         comandos[0] -> "transform"
+         comandos[1] -> "input file name"
+         comandos[2] -> "output file name"
+         comandos[3] em diante -> nome dos filtros a aplicar
+   */
+
+// Função que dado o nome do filtro retorna o nome do executavel
+char* filter_diretory(char* filter, FILTROS filtros[]){
+    int i = 0;
+    int val = 0;
+    while(i < filtros[0]->numero_filtros && val == 0){
+        if(strcmp(filter, filtros[i]->filtro_name) == 0) val = 1;
+        i++;
+    }
+
+    if(val == 1) return filtros[i-1]->filtro_exec;
+    else return NULL;
+}
+
+// Função de execução do comando transform
+void exec_transform(char* config_filename, char* filters_folder, char** comandos, FILTROS filtros[]){
+
+    int numFilters = numisFilter(comandos, filtros); // numero de filtros a serem aplicados à musica dada
+    
+    int fi = open(comandos[1], O_RDONLY);   // descritor musica a ser tratada
+    int fo = open(comandos[2], O_WRONLY | O_CREAT | O_TRUNC, 0644); // descritor da musica final
+
+    dup2(fi, 0);  // redirecionamentos
+    dup2(fo, 1);
+
+    close(fi);
+    close(fo);
+
+    int fd[2];
+    int i;
+    for(i = 0; i < numFilters - 1; i++) {  // execução dos vários filtros seguidos
+        int pipe_ret = pipe(fd);
+        if(pipe_ret == -1){
+            perror("Problema ao criar o pipe!");
+            return;
+        }
+
+        if(fork() == 0){
+             dup2(fd[1], 1);
+             close(fd[1]);  // fd[0] leitura, fd[1] escrita
+             close(fd[0]);
+             char* diretoria = malloc(1024);
+             strcpy(diretoria, filters_folder);
+             strcat(diretoria, "/");
+             char* exec_filter = filter_diretory(comandos[3+i], filtros); // vai a estrutura de dados buscar o nome do executavel
+             if (exec_filter == NULL){
+                 perror("Filtro nao existente");
+                 return;
+             } 
+             strcat(diretoria, exec_filter);          // fazer função para atribuir as diretorias dos executaveis aos respetivos nomes dos filtros
+             execl(diretoria, diretoria, NULL);
+        } else { 
+             dup2(fd[0], 0);
+             close(fd[0]);
+             close(fd[1]);
+        }    
+    }    
+    // ultimo caso, redirecionamento para o ficheiro de output
+    if(fork() == 0){
+    char* diretoria = malloc(1024);
+    strcpy(diretoria, filters_folder);
+    strcat(diretoria, "/");
+    char* exec_filter = filter_diretory(comandos[3+i], filtros);
+    if (exec_filter == NULL){
+            perror("Filtro nao existente");
+            return;
+    } 
+    strcat(diretoria, exec_filter);
+    execl(diretoria, diretoria, NULL);
+    }
+}
+
+// Função para contar o numero de filtros definidos no conf com base em \n
+int numberFilters(char* filtros){
+
+    int i = 0;
+    int cont = 0;
+    while(filtros[i]){
+        if(filtros[i] == '\n') cont++;
+        i++;
+    }
+    return cont;
+}
+
+// Função para inicializar a estrutura de filtros com base no ficheiro de configuração de filtros
+FILTROS* initFiltros(char* config_filename){
+    
+    
+    int fd = open(config_filename, O_RDONLY); // descritor para o ficheirod e configuração
+
+    if (fd == -1){
+        write(1, "\nCould not open the config file\n", 32); // tratamento de erro
     }
     
-    if (strcmp(comandos[0]))
+    char* buffer = malloc(sizeof(char) * 1024);
+    
+    read(fd,buffer,1024);                       // le o conteudo do ficheiro de configuração        
+    int numFilters = numberFilters(buffer);     // determina o numero de filtros existentes com base nos \n
+    
+    FILTROS *filtros = malloc(sizeof(FILTROS)* numFilters);
 
+    char* configs[numFilters][3];
+   
+    for(int j = 0; j < numFilters ; j++){       // faz a respetiva atribuição da divisão da string para uma matriz
+        for(int i = 0; i < 3 ; i++)
+            configs[j][i] = strdup(strsep(&buffer," \n"));
+    }
 
+    int i = 0;
+    while(i < numFilters){                                  // através da matriz anterior é inicializada a estrutura de filtros
+        filtros[i] = malloc(sizeof(struct filtros));
+        filtros[i]->filtro_name = strdup(configs[i][0]);
+        filtros[i]->max_used = atoi(configs[i][2]);
+        filtros[i]->used = 0;
+        filtros[i]->filtro_exec = strdup(configs[i][1]);
+        filtros[i]->numero_filtros = numFilters;
+        i++;
+    }
+
+    close(fd);
+
+    return filtros;
+}
+
+void exec_status(FILTROS filtros[]){ // Incompleto, falta meter as tasks em processamento
+    int i = 0;
+    while(i < filtros[0]->numero_filtros){
+        char comando[2000] = "";
+        strcat(comando, "filter");
+        strcat(comando, " ");
+        strcat(comando, filtros[i]->filtro_name);
+        strcat(comando, ":");
+        strcat(comando, " ");
+        char buffer[10];
+        sprintf(buffer,"%d", filtros[i]->used);
+        strcat(comando, buffer);
+        strcat(comando, "/");
+        sprintf(buffer,"%d", filtros[i]->max_used);
+        strcat(comando, buffer);
+        strcat(comando, " ");
+        strcat(comando, "(running/max)");
+        strcat(comando, "\n");
+        write(1, comando, strlen(comando));
+        i++;
+    }
+}
+
+int main (int argc, char * argv[]){ 
+
+    if(argc != 3){                              // caso a chamada de inicialização do servidor falhe
+        write(1, "Argumetos Inválidos", 21);
+        return 0;
+    }
+    //etc/aurrasd.config                        // estes são os ficheiros que o professor deu para testes
+    //bin/aurrasd-filters
+    char* config_filename = strdup(argv[1]);
+    char* filters_folder = strdup(argv[2]);
+    
+        
+    FILTROS *filtros = initFiltros(config_filename);  // isto tem que se alterar pois aqui ainda nao sabemos quantos filtros iremos ter
+                                         // inicialização dos filtors
+
+      // abertura do fifo de comunicação
+
+    char* buffer = malloc(1024);
+    int bytes_read = 0;
+    
+    while(1){                                       // ciclo infinito em que o servidor vai estar constantemente a ler do fifo
+    int fd = open("bin/clientetoserver", O_RDONLY);
+    bytes_read = read(fd, buffer, 1024);
+
+    if(bytes_read > 0){
+        char** comandos = inputDivide(buffer); // duas possiveis chamadas ao servidor
+                                               // a aplicação de filtros a uma musica
+        if (strcmp(comandos[0], "transform") == 0) exec_transform(config_filename, filters_folder, comandos, filtros);
+                                               // quando o cliente pede o status do servidor
+      
+        if (strcmp(comandos[0], "status") == 0) exec_status(filtros); // fazer este comando
+        
+        free(comandos);
+       }
+    }
+    
     return 0;
 }
+
 
 
 
